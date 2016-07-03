b76fb1a87dfec0c85fc04a475e9ef47899d1e8b3
ivoryworks <ivoryworkz@gmail.com>
2016-07-04 08:41:42 +0900

    [modify] Android AppからRealtimeDatabaseへ書き込み

--- Android/FireCtrlR/app/src/main/java/com/ivoryworks/firectrlr/MainActivity.java
+++ Android/FireCtrlR/app/src/main/java/com/ivoryworks/firectrlr/MainActivity.java
@@ -3,11 +3,26 @@ package com.ivoryworks.firectrlr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        writeKeyCode(0x0A);
+    }
+
+    private void writeKeyCode(int keyCode) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference keyCodeRef = database.getReference("key_code");
+        keyCodeRef.setValue(keyCode);
+
+        DatabaseReference timeRef = database.getReference("time_stamp");
+        timeRef.setValue(System.currentTimeMillis());
     }
 }
