684820684161948d1a58f7b4bfbab291df3f3058
ivoryworks <ivoryworkz@gmail.com>
2016-07-08 08:09:34 +0900

    Databaseの構造を変更

---
+++ Android/FireCtrlR/app/src/main/java/com/ivoryworks/firectrlr/FCKeyEvent.java
@@ -0,0 +1,27 @@
+package com.ivoryworks.firectrlr;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FCKeyEvent {
+
+    public long mTimeStamp;
+    public int mKeyCode;
+
+    public FCKeyEvent() {
+
+    }
+
+    public FCKeyEvent(long timeStamp, int keyCode ) {
+        this.mTimeStamp = timeStamp;
+        this.mKeyCode = keyCode;
+    }
+
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("time_stamp", mTimeStamp);
+        result.put("key_code", mKeyCode);
+
+        return result;
+    }
+}
--- Android/FireCtrlR/app/src/main/java/com/ivoryworks/firectrlr/MainActivity.java
+++ Android/FireCtrlR/app/src/main/java/com/ivoryworks/firectrlr/MainActivity.java
@@ -1,6 +1,5 @@
 package com.ivoryworks.firectrlr;
 
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,12 +7,18 @@ import android.view.View;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         findViewById(R.id.btn_0A).setOnClickListener(this);
         findViewById(R.id.btn_0B).setOnClickListener(this);
@@ -23,21 +28,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_0A:
-                writeKeyCode(0x0A);
+                writeKeyEvent(0x0A);
                 break;
             case R.id.btn_0B:
-                writeKeyCode(0x0B);
+                writeKeyEvent(0x0B);
                 break;
         }
     }
 
-    private void writeKeyCode(int keyCode) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-        DatabaseReference keyCodeRef = database.getReference("key_code");
-        DatabaseReference timeRef = database.getReference("time_stamp");
-
-        keyCodeRef.setValue(keyCode);
-        timeRef.setValue(System.currentTimeMillis());
+    private void writeKeyEvent(int keyCode) {
+        FCKeyEvent keyEvent = new FCKeyEvent(System.currentTimeMillis(), keyCode);
+        Map<String, Object> postValues = keyEvent.toMap();
+        mDatabase.child("key_event").setValue(postValues);
     }
 }
